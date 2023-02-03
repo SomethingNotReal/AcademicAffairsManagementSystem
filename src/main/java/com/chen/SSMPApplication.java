b9f923a5994e22c6781c1909173a779db57995f8
志聪 <chen2360581534@gmail.com>
2023-02-03 16:01:50 +0800

    init

--- src/main/java/com/chen/SSMPApplication.java
+++ src/main/java/com/chen/SSMPApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademicAffairsManagementSystemApplication {
+public class SSMPApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AcademicAffairsManagementSystemApplication.class, args);
+        SpringApplication.run(SSMPApplication.class, args);
     }
 
 }
